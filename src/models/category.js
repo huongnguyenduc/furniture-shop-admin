b18a4c002945c7564029be96698c1cf4c05de56b
Bui Minh Huy <buiminhhuyqnam@gmail.com>
2022-04-07 10:53:05 +0700

    complete UI add product

---
+++ src/models/category.js
@@ -0,0 +1,18 @@
+export default {
+    namespace: 'category',
+    state: {
+        categories: [
+            {
+                id:1,
+                name: 'Laptop',
+                description: 'cc',
+                options: [
+                    'Vi xử lý',
+                    'RAM',
+                    'Độ phân giải',
+                    'Bộ nhớ',
+                ]
+            }
+        ]
+    }
+}
